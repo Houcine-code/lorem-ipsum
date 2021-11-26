2157ac8674ca28e7296022a8e146671219db31df
Houcine-code <houc.bh90@gmail.com>
2021-11-26 23:16:35 +0100

    Updates

---
+++ precache-manifest.eedca5c95de2b5428b186fc5331cd722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec686c80c4d89798dedbf02be45ae10",
+    "url": "/lorem-ipsum/index.html"
+  },
+  {
+    "revision": "c2200adab6f109e21994",
+    "url": "/lorem-ipsum/static/css/main.fd5b502b.chunk.css"
+  },
+  {
+    "revision": "89874f33811477ce27e9",
+    "url": "/lorem-ipsum/static/js/2.6d334d6c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/lorem-ipsum/static/js/2.6d334d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2200adab6f109e21994",
+    "url": "/lorem-ipsum/static/js/main.1612dc4f.chunk.js"
+  },
+  {
+    "revision": "6615968d0b1ae8088015",
+    "url": "/lorem-ipsum/static/js/runtime-main.b8f9d5e5.js"
+  }
+]);
